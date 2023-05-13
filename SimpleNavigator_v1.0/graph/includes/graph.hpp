d784f1cf01065ff91df6511441bc2bc3240ed07c
Mera Salena <msalena@vo-e4.msk.21-school.ru>
2023-05-13 17:41:53 +0300

    [msalena] Graph::ExportGraphToDo: implemented the structure

--- SimpleNavigator_v1.0/graph/includes/graph.hpp
+++ SimpleNavigator_v1.0/graph/includes/graph.hpp
@@ -1,19 +1,42 @@
 #pragma once
 
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include <vector>
+
+namespace fs = std::filesystem;
+
 namespace s21{
+
+const fs::path ROOT_DIR = fs::current_path();
+const fs::path GRAPHS_PATH = "materials/graphs";
+const fs::path DOTS_PATH = "materials/dots_representation";
 
 class Graph{
 public:
-    Graph();
-    Graph(const Graph& other);
-    Graph(Graph&& other);
-    ~Graph();
+    using graph_type    = std::vector<std::vector<int>>;
 
-    Graph& operator=(const Graph& other);
-    Graph& operator=(Graph&& other);
+    Graph();
+    Graph(const Graph& other) = default;
+    Graph(Graph&& other) = default;
+    ~Graph() = default;
+
+    Graph& operator=(const Graph& other) = default;
+    Graph& operator=(Graph&& other) = default;
+
+    void ExportGraphToDot(std::string filename);
 
 private:
+    graph_type graph_;
+    bool is_directed_;
 
+    fs::path createFileIfNotExist_(std::string& filename);
+
+    std::string dotFilename_(std::string& filename);
+
+    std::string graphDotRepresentation_();
 };
 
 }
--- SimpleNavigator_v1.0/graph/srcs/graph.cpp
+++ SimpleNavigator_v1.0/graph/srcs/graph.cpp
@@ -2,26 +2,32 @@
 
 namespace s21{
 
-Graph::Graph(){
+Graph::Graph() : is_directed_(0){ }
 
-}
-Graph::Graph(const Graph& other){
-    (void)other;
-}
-Graph::Graph(Graph&& other){
-    (void)other;
-}
-Graph::~Graph(){
+void Graph::ExportGraphToDot(std::string filename){
+    fs::path result_path = std::move(createFileIfNotExist_(filename));
+    std::ofstream dot_file(result_path.native());
+    std::string dot_graph = graphDotRepresentation_();
 
+    dot_file << dot_graph << std::endl;
 }
 
-Graph& Graph::operator=(const Graph& other){
-    (void)other;
-    return *this;
+fs::path Graph::createFileIfNotExist_(std::string& filename){
+    fs::path result_path = ROOT_DIR / DOTS_PATH / dotFilename_(filename);
+
+    if(!fs::exists(result_path)){
+        fs::create_directories(result_path);
+    }
+
+    return result_path;
 }
-Graph& Graph::operator=(Graph&& other){
-    (void)other;
-    return *this;
+
+std::string Graph::dotFilename_(std::string& filename){
+
+}
+
+std::string Graph::graphDotRepresentation_(){
+
 }
 
 }
