ba3d95ae5dfcff367ef6f2120d3b7e1659835b54
mavmi <matveev.ivmi@yandex.ru>
2023-06-05 21:48:16 +0300

    [pmaryjoP6] Added some graphs and tests

--- SimpleNavigator_v1.0/tests/graph/srcs/graphAlgorithms.cpp
+++ SimpleNavigator_v1.0/tests/graph/srcs/graphAlgorithms.cpp
@@ -2,10 +2,11 @@
 
 namespace s21::tests::graph{
 
-TEST(TEST_SUITE_NAME, BreadthFirstSearch){
+TEST(TEST_SUITE_NAME, BreadthFirstSearch1){
+    const int size = 11;
     s21::Graph graph;
     s21::GraphAlgorithms graphAlgorithms;
-    const std::string answ[11] = {
+    const std::string answ[size] = {
         "1 2 3 4 5 6 7 8 9 10 11",
         "2 1 3 4 5 6 7 8 9 10 11",
         "3 1 2 4 5 6 7 8 9 10 11",
@@ -32,46 +33,234 @@ TEST(TEST_SUITE_NAME, BreadthFirstSearch){
     
     graph.LoadGraphFromFile("./tests/materials/graph/valid/1");
     
-    for (int i = 0; i < 11; i++){
+    for (int i = 0; i < size; i++){
         ASSERT_EQ(vectorToString(graphAlgorithms.BreadthFirstSearch(graph, i)),
                    answ[i]);
     }
 }
 
-TEST(TEST_SUITE_NAME, MinimumSpanningTree){
+TEST(TEST_SUITE_NAME, BreadthFirstSearch2){
+    const int size = 6;
+    s21::Graph graph;
     s21::GraphAlgorithms graphAlgorithms;
-    
-    {
-        s21::Graph graph;
-        ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/1"));
-        s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
-        ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 150);
-    }
+    const std::string answ[size] = {
+        "1 2 3 6 4 5",
+        "2 1 3 4 6 5",
+        "3 1 2 4 6 5",
+        "4 2 3 5 1 6",
+        "5 4 6 2 3 1",
+        "6 1 3 5 2 4"
+    };
 
-    {
-        s21::Graph graph;
-        ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/2"));
-        s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
+    auto vectorToString = [](const std::vector<int>& v){
+        std::string str;
         
-        ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 33);
-        for (s21::Graph::size_type i = 0; i < minSpanningTree.Size(); i++){
-        for (s21::Graph::size_type j = 0; j < minSpanningTree.Size(); j++){
-            int val = minSpanningTree.at(i, j);
-            if (i == 0 && j == 1 && val == 7) ASSERT_TRUE(true);
-            else if (i == 1 && j == 0 && val == 7) ASSERT_TRUE(true);
-            else if (i == 0 && j == 2 && val == 9) ASSERT_TRUE(true);
-            else if (i == 2 && j == 0 && val == 9) ASSERT_TRUE(true);
-            else if (i == 3 && j == 4 && val == 6) ASSERT_TRUE(true);
-            else if (i == 4 && j == 3 && val == 6) ASSERT_TRUE(true);
-            else if (i == 2 && j == 5 && val == 2) ASSERT_TRUE(true);
-            else if (i == 5 && j == 2 && val == 2) ASSERT_TRUE(true);
-            else if (i == 4 && j == 5 && val == 9) ASSERT_TRUE(true);
-            else if (i == 5 && j == 4 && val == 9) ASSERT_TRUE(true);
-            else if (val == 0) ASSERT_TRUE(true);
-            else ASSERT_TRUE(false);
+        for (size_t i = 0; i < v.size(); i++){
+            str += std::to_string(v[i] + 1);
+            if (i + 1 != v.size()) str += ' ';
         }
-        }
+
+        return str;
+    };
+    
+    graph.LoadGraphFromFile("./tests/materials/graph/valid/2");
+    
+    for (int i = 0; i < size; i++){
+        ASSERT_EQ(vectorToString(graphAlgorithms.BreadthFirstSearch(graph, i)),
+                   answ[i]);
     }
+}
+
+TEST(TEST_SUITE_NAME, BreadthFirstSearch3){
+    const int size = 6;
+    s21::Graph graph;
+    s21::GraphAlgorithms graphAlgorithms;
+    const std::string answ[size] = {
+        "1 2 4 3 5 6",
+        "2 3 1 6 4 5",
+        "3 1 6 2 4 5",
+        "4 5 3 1 6 2",
+        "5 3 4 1 6 2",
+        "6 5 3 4 1 2"
+    };
+
+    auto vectorToString = [](const std::vector<int>& v){
+        std::string str;
+        
+        for (size_t i = 0; i < v.size(); i++){
+            str += std::to_string(v[i] + 1);
+            if (i + 1 != v.size()) str += ' ';
+        }
+
+        return str;
+    };
+    
+    graph.LoadGraphFromFile("./tests/materials/graph/valid/3");
+    
+    for (int i = 0; i < size; i++){
+        ASSERT_EQ(vectorToString(graphAlgorithms.BreadthFirstSearch(graph, i)),
+                   answ[i]);
+    }
+}
+
+TEST(TEST_SUITE_NAME, BreadthFirstSearch4){
+    const int size = 8;
+    s21::Graph graph;
+    s21::GraphAlgorithms graphAlgorithms;
+    const std::string answ[size] = {
+        "1 2 5 3 6 4 7 8",
+        "2 3 4 5 6 7 8",
+        "3 2 4 5 6 7 8",
+        "4 5 6 3 7 2 8",
+        "5 6 3 7 2 4 8",
+        "6 3 7 2 4 8 5",
+        "7 6 8 3 2 4 5",
+        "8 6 3 7 2 4 5"
+    };
+
+    auto vectorToString = [](const std::vector<int>& v){
+        std::string str;
+        
+        for (size_t i = 0; i < v.size(); i++){
+            str += std::to_string(v[i] + 1);
+            if (i + 1 != v.size()) str += ' ';
+        }
+
+        return str;
+    };
+    
+    graph.LoadGraphFromFile("./tests/materials/graph/valid/4");
+    
+    for (int i = 0; i < size; i++){
+        ASSERT_EQ(vectorToString(graphAlgorithms.BreadthFirstSearch(graph, i)),
+                   answ[i]);
+    }
+}
+
+TEST(TEST_SUITE_NAME, BreadthFirstSearch5){
+    const int size = 8;
+    s21::Graph graph;
+    s21::GraphAlgorithms graphAlgorithms;
+    const std::string answ[size] = {
+        "1 2 5 7 3 4",
+        "2 3 4",
+        "3 2 4",
+        "4",
+        "5 1 2 7 3 4",
+        "6 4",
+        "7 5 1 2 3 4",
+        "8"
+    };
+
+    auto vectorToString = [](const std::vector<int>& v){
+        std::string str;
+        
+        for (size_t i = 0; i < v.size(); i++){
+            str += std::to_string(v[i] + 1);
+            if (i + 1 != v.size()) str += ' ';
+        }
+
+        return str;
+    };
+    
+    graph.LoadGraphFromFile("./tests/materials/graph/valid/5");
+    
+    for (int i = 0; i < size; i++){
+        ASSERT_EQ(vectorToString(graphAlgorithms.BreadthFirstSearch(graph, i)),
+                   answ[i]);
+    }
+}
+
+TEST(TEST_SUITE_NAME, BreadthFirstSearch6){
+    const int size = 8;
+    s21::Graph graph;
+    s21::GraphAlgorithms graphAlgorithms;
+    const std::string answ[size] = {
+        "1 2 8 3 4 7 6 5",
+        "2 3 4 6 5 8 7",
+        "3 4 6 5 8 7",
+        "4 5 3 7 6 8",
+        "5 3 7 4 6 8",
+        "6 8 7",
+        "7 6 8",
+        "8 7 6"
+    };
+
+    auto vectorToString = [](const std::vector<int>& v){
+        std::string str;
+        
+        for (size_t i = 0; i < v.size(); i++){
+            str += std::to_string(v[i] + 1);
+            if (i + 1 != v.size()) str += ' ';
+        }
+
+        return str;
+    };
+    
+    graph.LoadGraphFromFile("./tests/materials/graph/valid/6");
+    
+    for (int i = 0; i < size; i++){
+        ASSERT_EQ(vectorToString(graphAlgorithms.BreadthFirstSearch(graph, i)),
+                   answ[i]);
+    }
+}
+
+TEST(TEST_SUITE_NAME, MinimumSpanningTree1){
+    s21::GraphAlgorithms graphAlgorithms;
+    s21::Graph graph;
+    ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/1"));
+    s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
+    ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 150);
+}
+
+TEST(TEST_SUITE_NAME, MinimumSpanningTree2){
+    s21::GraphAlgorithms graphAlgorithms;
+    s21::Graph graph;
+    ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/2"));
+    s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
+    
+    ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 33);
+    for (s21::Graph::size_type i = 0; i < minSpanningTree.Size(); i++){
+    for (s21::Graph::size_type j = 0; j < minSpanningTree.Size(); j++){
+        int val = minSpanningTree.at(i, j);
+        if (i == 0 && j == 1 && val == 7) ASSERT_TRUE(true);
+        else if (i == 1 && j == 0 && val == 7) ASSERT_TRUE(true);
+        else if (i == 0 && j == 2 && val == 9) ASSERT_TRUE(true);
+        else if (i == 2 && j == 0 && val == 9) ASSERT_TRUE(true);
+        else if (i == 3 && j == 4 && val == 6) ASSERT_TRUE(true);
+        else if (i == 4 && j == 3 && val == 6) ASSERT_TRUE(true);
+        else if (i == 2 && j == 5 && val == 2) ASSERT_TRUE(true);
+        else if (i == 5 && j == 2 && val == 2) ASSERT_TRUE(true);
+        else if (i == 4 && j == 5 && val == 9) ASSERT_TRUE(true);
+        else if (i == 5 && j == 4 && val == 9) ASSERT_TRUE(true);
+        else if (val == 0) ASSERT_TRUE(true);
+        else ASSERT_TRUE(false);
+    }
+    }
+}
+
+TEST(TEST_SUITE_NAME, MinimumSpanningTree3){
+    s21::GraphAlgorithms graphAlgorithms;
+    s21::Graph graph;
+    ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/3"));
+    s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
+    ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 66);
+}
+
+TEST(TEST_SUITE_NAME, MinimumSpanningTree4){
+    s21::GraphAlgorithms graphAlgorithms;
+    s21::Graph graph;
+    ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/4"));
+    s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
+    ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 87);
+}
+
+TEST(TEST_SUITE_NAME, MinimumSpanningTree6){
+    s21::GraphAlgorithms graphAlgorithms;
+    s21::Graph graph;
+    ASSERT_TRUE(graph.LoadGraphFromFile("./tests/materials/graph/valid/6"));
+    s21::Graph minSpanningTree = graphAlgorithms.GetLeastSpanningTree(graph);
+    ASSERT_EQ(minSpanningTree.MinSpanningTreeSize(), 70);
 }
 
 TEST(TEST_SUITE_NAME, GetShortestPathBetweenVertices){
