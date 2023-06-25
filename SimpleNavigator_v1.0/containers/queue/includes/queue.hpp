675abf12490670e4276abfbd189ff4bac3b4dde2
Mera Salena <msalena@mi-d3.msk.21-school.ru>
2023-06-25 19:36:33 +0300

    [develop] comments: added comments for queue

--- SimpleNavigator_v1.0/containers/queue/includes/queue.hpp
+++ SimpleNavigator_v1.0/containers/queue/includes/queue.hpp
@@ -22,16 +22,33 @@ public:
     Queue& operator=(const Queue& other);
     Queue& operator=(Queue&& other);
 
+    // Adding an element by reference
     void push(const value_type& value);
 
+    // Adding an element by r-value reference
     void push(value_type&& value);
 
+    /**
+     * Getting an element from the queue
+     * followed by its removal from the queue
+     */
     void pop();
 
+    /**
+     * Accessing the first element
+     * @return reference to the first element
+     */
     reference front();
 
+    /**
+     * Accessing the first element
+     * @return const reference to the first element
+     */
     const_reference front() const;
 
+     /**
+     * @return the number of elements
+     */
     size_type size() const;
 
 private:
