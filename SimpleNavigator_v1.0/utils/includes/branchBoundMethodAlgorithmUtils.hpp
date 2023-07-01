d31fc307dabdf624b8d1578d29cf9998fdb6cf13
Mera Salena <msalena@il-a1.msk.21-school.ru>
2023-07-01 14:29:49 +0300

    [develop] commenting:BbmaUtils: in process

--- SimpleNavigator_v1.0/utils/includes/branchBoundMethodAlgorithmUtils.hpp
+++ SimpleNavigator_v1.0/utils/includes/branchBoundMethodAlgorithmUtils.hpp
@@ -18,6 +18,12 @@ class PathNodeNotIncludeMatrix;
 
 using node_shared_ptr   = std::shared_ptr<PathNodeRootMatrix>;
 
+/**
+ * Compare function for multiset with tree Nodes which didn't forked still. 
+ * Compare way_cost of [a] and [b] tree Nodes during adding their to multiset 
+ * @return true if [a].cost_way < [b].cost_way
+ * @return false if [a].cost_way >= [b].cost_way
+ */
 bool NodesCostCompare(const node_shared_ptr& a, const node_shared_ptr& b);
 
 class BbmaUtils{
@@ -26,9 +32,19 @@ public:
     using multiset_type = std::multiset<node_shared_ptr,
                             decltype(NodesCostCompare)*>;
 
+    /**
+     * Creating the new restructed Node's matrix based on [graph].matrix. 
+     * Every cell is std::pair with default cell's coordinate and weight. 
+     * The weight is set as infinity(max_int) if default value of weight  
+     * is zero or there is from_node == to_node
+     * @return matrix_pair_unique_ptr to new Node's matrix
+     */
     matrix_pair_unique_ptr InitialMatrix(const Graph& graph);
 
-    // return itertor to added PathNodeIncludeMatrix
+    /**
+     * Adding new unforkted [matrix_node] to multiset [unforked_nodes]
+     * @return multiset_type::iterator to added PathNodeIncludeMatrix
+     */
     multiset_type::iterator AddWayNodesToUnforkedNodes(
                                 multiset_type& unforked_nodes,
                                 PathNodeRootMatrix& matrix_node);
